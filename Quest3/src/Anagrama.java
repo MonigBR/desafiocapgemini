18f8597ec9152a1f04e32570d4e7d2cafb85282e
GreemerBR <83251745+MonigBR@users.noreply.github.com>
2022-02-17 23:17:37 -0300

    Desenvolvimento Questão 3

---
+++ Quest3/src/Anagrama.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+	
+public class Anagrama {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);	
+		System.out.println();
+		System.out.print("Digite o conjunto de caracteres para saber quantos anagramas apartir dos subconjutos formados serão gerados: ");
+		String conjunto = sc.next();	
+		anagramas(conjunto);	
+		System.out.println();	
+		sc.close();
+		}
+	
+	public static void anagramas(String conjunto) {
+		HashMap<String, Integer> sub = new HashMap<>();	
+		int anagramas = 0;
+		for(int i=0; i<conjunto.length(); i++) {
+			
+			for(int j=i; j<conjunto.length(); j++) {
+			
+				char[] novoConjuntoChar = conjunto.substring(i, j+1).toCharArray();
+				Arrays.sort(novoConjuntoChar);
+				String novoConjunto = new String(novoConjuntoChar);
+				
+				if(sub.containsKey(novoConjunto)) {				
+					sub.put(novoConjunto, sub.get(novoConjunto)+1);
+					}
+				
+				else {
+					sub.put(novoConjunto, 1);
+					}
+				}
+			}
+		
+		for(String novoConjunto:sub.keySet()) {
+			int valor = sub.get(novoConjunto);
+			if(valor == 1) {
+				anagramas += 0;
+				}
+				
+			else {
+				anagramas += 1;			
+				}
+			}
+			
+		System.out.println();
+		System.out.printf("Existem %d anagrama(s) do conjunto de caracteres %s.", anagramas, conjunto);
+	}
+}
